6fda28f1c1d844cf14d508484f52eb7852647232
ozerikoz <erik.paschoalatto1@gmail.com>
2024-11-04 12:35:19 -0300

    fix client_flask

--- client_flask.py
+++ client_flask.py
@@ -1,29 +1,32 @@
-# import requests
-# import json
+import requests
+import json
 
-# def diagnostic(data):
-#     url = 'http://127.0.0.1:5000/diagnostic'
-#     headers = {'Content-type': 'application/json'}
+def diagnostic(data):
+    url = 'http://127.0.0.1:5000/diagnostic'
+    headers = {'Content-type': 'application/json'}
     
-#     # Enviar a solicitação POST com os dados da nova amostra
-#     response = requests.post(url, data=json.dumps(data), headers=headers)
+    # Enviar a solicitação POST com os dados da nova amostra
+    response = requests.post(url, data=json.dumps(data), headers=headers)
     
-#     # Verificar se a solicitação foi bem-sucedida
-#     if response.status_code == 200:
-#         result = response.json()
-#         prediction = result['prediction']
-#         print(f"Previsão para a nova amostra: {prediction}")
-#     else:
-#         print("Erro ao fazer a previsão.")
+    # Verificar se a solicitação foi bem-sucedida
+    if response.status_code == 200:
+        result = response.json()
 
-# if __name__ == '__main__':
-#     # Dados da nova amostra
-#     nova_amostra = {
-#         "problema": "motor esquentando",
-#         "fabricante": "Toyota",
-#         "modelo": "Corolla",
-#         "ano": 2018
-#     }
+        diagnostic = result['resultado']
+        json_formated = json.dumps(diagnostic, indent=4, ensure_ascii=False)
+        
+        print(f"\nResultado do diagnostico: {json_formated}\n")
+    else:
+        print("Erro ao fazer a previsão.")
 
-#     # Realizar o diagnostico
-#     diagnostic(nova_amostra)
+if __name__ == '__main__':
+    # Dados da nova amostra
+    nova_amostra = {
+        "problema": "motor esquentando",
+        "fabricante": "Toyota",
+        "modelo": "Corolla",
+        "ano": 2018
+    }
+
+    # Realizar o diagnostico
+    diagnostic(nova_amostra)
