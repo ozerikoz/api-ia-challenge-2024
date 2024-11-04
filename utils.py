896786446667d561e9c8fd8b59a25492dc554e58
ozerikoz <erik.paschoalatto1@gmail.com>
2024-11-04 12:49:01 -0300

    teste deploy render

--- utils.py
+++ utils.py
@@ -1,9 +1,22 @@
 import re
 import unicodedata
 from nltk.corpus import stopwords
+import nltk
+import os
 
-# Configurar stopwords em português
-stop_words = set(stopwords.words('portuguese'))
+# Configurar o caminho para os dados do NLTK
+nltk_data_path = '/opt/render/nltk_data'
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+
+# Baixar stopwords apenas se necessário
+try:
+    stop_words = set(stopwords.words('portuguese'))
+except LookupError:
+    nltk.download('stopwords', download_dir=nltk_data_path)
+    stop_words = set(stopwords.words('portuguese'))
 
 def preprocess_text(text):
     # Converte para minúsculas
